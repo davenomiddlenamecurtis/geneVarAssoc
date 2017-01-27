14fc84224cbef96814e2d09f584a577d2f5c22cd
Dave Curtis <d.curtis@ucl.ac.uk>
2017-01-27 16:29:09 +0000

    increased size of lineBUff and tempBuff to 20000 for long annotations

--- src/geneVarParser.cpp
+++ src/geneVarParser.cpp
@@ -53,9 +53,11 @@ void weightTable::init(char *n,consequenceReport consequence[],int nConsequence)
 	weightTableList[tableName]=this;
 }
 
+char lineBuff[20000],tempBuff[20000]; // need these to be big for e.g. VEP on TTN
+
 dcexpr_val *performTabixQuery(const char *fn,int addChr,char *lookupStr)
 {
-	char fnBuff[1000],*ptr,*tptr,queryBuff[1000],lineBuff[10000],tempBuff[10000],chrStr[10];
+	char fnBuff[1000],*ptr,*tptr,queryBuff[1000],chrStr[10];
 	dcexpr_val *rv;
 	FILE *fq;
 	strcpy(fnBuff,fn);
