fa4b2a8f4071cfb14ffee385b7f5c20484609192
Dave Curtis <d.curtis@ucl.ac.uk>
2019-03-15 16:06:43 +0000

    Update reorderVCFSubs.cpp

--- src/reorderVCFSubs.cpp
+++ src/reorderVCFSubs.cpp
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define FIELDLENGTH 40
+#define FIELDLENGTH 60
 #define MAXSUBS 10000
 
 char line[FIELDLENGTH*(MAXSUBS + 20)], right[MAXSUBS + 20][FIELDLENGTH],wrong[MAXSUBS + 20][FIELDLENGTH];
