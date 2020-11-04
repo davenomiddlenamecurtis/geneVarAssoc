f0b13c6f2d66db6033b9f4fb00e03066648251af
Dave Curtis <davenomiddlenamecurtis@gmail.com>
2020-11-04 08:24:46 +0000

    Fixed function to count subjects

--- src/vcfLocusFile.cpp
+++ src/vcfLocusFile.cpp
@@ -380,6 +380,7 @@ int vcfLocusFile::readHeaderInfo()
 			return 0;
 		}
 	} while (strncmp(buff, "#CHROM", strlen("#CHROM")));
+	fseek(fp, fpos, SEEK_SET);
 	for (ch = fgetc(fp), sk = 0; sk < nFieldsToSkip; ++sk)
 	{
 		while (!isspace(ch))
