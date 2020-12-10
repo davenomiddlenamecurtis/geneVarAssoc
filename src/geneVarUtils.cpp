be2156e285d89b27078ce0ff8eaa131852e2eb9f
Dave Curtis <davenomiddlenamecurtis@gmail.com>
2020-12-10 15:06:43 +0000

    added --missingzero

--- src/geneVarUtils.cpp
+++ src/geneVarUtils.cpp
@@ -102,7 +102,10 @@ int gvaParams::readParms(int argc,char *argv[],analysisSpecs &spec)
 			dcerror(1,"Expected argument beginning -- but got this: %s\n",arg);
 			return 0;
 		}
-		else if (!strcmp(arg, "--dottest") || !strcmp(arg, "--dolrtest") || !strcmp(arg, "--dolinrtest") || !strcmp(arg, "--varfile") || !strcmp(arg, "--testfile") || !strcmp(arg, "--lintestfile") || !strcmp(arg, "--start-from-fitted") || !strcmp(arg, "--maxmaf"))
+		else if (!strcmp(arg, "--dottest") || !strcmp(arg, "--dolrtest")
+			|| !strcmp(arg, "--dolinrtest") || !strcmp(arg, "--varfile")
+			|| !strcmp(arg, "--testfile") || !strcmp(arg, "--lintestfile")
+			|| !strcmp(arg, "--start-from-fitted") || !strcmp(arg, "--maxmaf") || !strcmp(arg, "--missingzero"))
 		{
 			strcpy(spec.scoreassocArgs[spec.nScoreassocArgs][0], arg);
 			getNextArg(arg, argc, argv, fp, &depth, &argNum);
